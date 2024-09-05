b441e624c795ac5ace143f0efdb9b1877015997d
Bhupendra singh <bhupendrasingh05022003@gmail.com>
2024-09-05 22:36:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
-// const port = 3000
+const port = 3000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -15,6 +15,6 @@ app.get('/login', (req, res)=>{
     res.send('<h1>This is login</h1>')
 })
 
-app.listen(process.env.port, () => {
-  console.log(`Example app listening on port ${process.env.port}`)
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
 })
